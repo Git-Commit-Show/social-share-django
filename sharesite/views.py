2dc69c4f478755e9cd27a53019bdecfdd6904d44
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-20 11:58:39 +0530

    :sos: help me

--- sharesite/views.py
+++ sharesite/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .forms import PostForm
 import requests
 # Create your views here.
 def sharePage(request):
-    return render(request, 'sharePage.html')
-    '''
-	if request.method == "GET":
-		form = PostForm(request.GET)
-		data = request.GET.copy()
-		print(data.get('hashTags'))
-		if( form.is_valid()):
-			form.save()
-	else:
-		form = PostForm()
-	return render(request,'sharePage.html',{'text':request.GET['Text'],'hashtag':request.GET['hashTags'],'url':request.GET['URL'],'form':form})
-    '''
+    if request.method == "GET":
+        form = PostForm(request.GET)
+        data = request.GET.copy()
+        print(data.get('hashTags'))
+        if( form.is_valid()):
+            form.save()
+            return render(request,'sharePage.html',{'text':request.GET['Text'],'hashtag':request.GET['hashTags'],'url':request.GET['URL'],'form':form})
+    else:
+        form = PostForm()
+
+    return render(request,'sharePage.html',{'text':request.GET['Text'],'hashtag':request.GET['hashTags'],'url':request.GET['URL'],'form':form})
