84f046c46cdd99be9d1a978d83e616e3e7bfdf12
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-24 08:34:52 +0530

    Concatenated media dir path

--- waterMarker/views.py
+++ waterMarker/views.py
@@ -17,32 +17,33 @@ TEXT_POSITION = "bc"
 TEXT_ALIGN_WATERMARK = "r"
 # Create your views here.
 def upload(request):
-	logoPath=''
-	form= ImageForm(request.POST or None, request.FILES or None)
-	if(request.method=='POST'):
-		if form.is_valid():
-			form.save()
-			initial_obj = form.save(commit=False)
-			initial_obj.save()
-			logoPath=initial_obj.logo.name
-			imdt=logoPath.split('/')
-			logo=imdt[1]
-			print(os.getcwd())
-			try:
-			    print(os.getcwd())
-			    os.chdir('media/images')
-			    if(request.POST['path']):
-			    	image.watermark_a_folder(request.POST['path'])
-			    image.watermark(INPUT,OUTPUT,logo,request.POST['position'],request.POST['text'])
-			    print('It worked')
-			except Exception as e:
-				print(e)
-				print("Not working")
-	context= {'form': form}
-	return render(request,'upload.html',context)
+    logoPath=''
+    form= ImageForm(request.POST or None, request.FILES or None)
+    if(request.method=='POST'):
+            if form.is_valid():
+                    form.save()
+                    initial_obj = form.save(commit=False)
+                    initial_obj.save()
+                    logoPath=initial_obj.logo.name
+                    imdt=logoPath.split('/')
+                    logo=imdt[1]
+                    print(os.getcwd())
+                    try:
+                        print(os.getcwd())
+                        os.chdir('media/images')
+                        if(request.POST['path']):
+                            image.watermark_a_folder(request.POST['path'])
+                        image.watermark(INPUT,OUTPUT,logo,request.POST['position'],request.POST['text'])
+                        print('It worked')
+                    except Exception as e:
+                            print(e)
+                            print("Not working")
+    context= {'form': form}
+    return render(request,'upload.html',context)
 
 
 def viewAll(request):
-	list=os.listdir('media/images/output')
-	return render(request,'view.html',{'list':list})
+    os.path.join(os.getcwd(), "media/images/output")
+    list=os.listdir('media/images/output')
+    return render(request,'view.html',{'list':list})
 
