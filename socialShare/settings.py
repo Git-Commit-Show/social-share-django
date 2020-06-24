5de12c3fa93ffc57a3bb6d5001bb1fd4c924b572
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-24 08:51:27 +0530

    ALLOWED_HOSTS Added

--- socialShare/settings.py
+++ socialShare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dv#trzf#rl!^uk=iqi*!s6#lh_-t$yln9)5_43d+6($#1!pp*a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.22.211.7"]
 
 
 # Application definition
