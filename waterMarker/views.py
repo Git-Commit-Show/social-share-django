9d6add5e49d71a90abd07e76e778181969b6af07
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-24 19:20:24 +0530

    Fixed indentation

--- waterMarker/views.py
+++ waterMarker/views.py
@@ -17,36 +17,36 @@ TEXT_POSITION = "bc"
 TEXT_ALIGN_WATERMARK = "r"
 # Create your views here.
 def upload(request):
-	logoPath=''
-	form= ImageForm(request.POST or None, request.FILES or None)
-	if(request.method=='POST'):
-		if form.is_valid():
-			form.save()
-			initial_obj = form.save(commit=False)
-			initial_obj.save()
-			logoPath=initial_obj.logo.name
-			imdt=logoPath.split('/',1)
-			logo=imdt[1]
-                        imdt1=imdt[1].split('/')
-			print(logo)
-			print(os.getcwd())
-                        OUTPUT='static/result_'+imdt1[1]
-			try:
-			    print(os.getcwd())
-			    os.chdir('media/images')
-			    if(request.POST['path']):
-			    	image.watermark_a_folder(request.POST['path'])
-			    image.watermark(INPUT,OUTPUT,logo,request.POST['position'],request.POST['text'])
-			    print('It worked')
-			except Exception as e:
-				print(e)
-				print("Not working")
-	context= {'form': form}
-	return render(request,'upload.html',context)
+    logoPath=''
+    form= ImageForm(request.POST or None, request.FILES or None)
+    if(request.method=='POST'):
+        if form.is_valid():
+            form.save()
+            initial_obj = form.save(commit=False)
+            initial_obj.save()
+            logoPath=initial_obj.logo.name
+            imdt=logoPath.split('/',1)
+            logo=imdt[1]
+            imdt1=imdt[1].split('/')
+            print(logo)
+            print(os.getcwd())
+            OUTPUT='static/result_'+imdt1[1]
+            try:
+                print(os.getcwd())
+                os.chdir('media/images')
+                if(request.POST['path']):
+                    image.watermark_a_folder(request.POST['path'])
+                image.watermark(INPUT,OUTPUT,logo,request.POST['position'],request.POST['text'])
+                print('It worked')
+            except Exception as e:
+                print(e)
+                print("Not working")
+    context= {'form': form}
+    return render(request,'upload.html',context)
 
 
 def viewAll(request):
-	print(os.getcwd())
-	list=os.listdir('static/')
-	return render(request,'view.html',{'list':list})
+    print(os.getcwd())
+    list=os.listdir('static/')
+    return render(request,'view.html',{'list':list})
 
